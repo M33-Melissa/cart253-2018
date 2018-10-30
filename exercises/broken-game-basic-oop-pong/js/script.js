9048f56f03975b45139059676197c557c2a9c665
Melissa-Lim <M33-Melissa@users.noreply.github.com>
2018-10-30 16:38:55 -0400

    Ex6: FIX(script.js) undetected Syntax error missing reference line 54 (corrected to ball.reset())

--- exercises/broken-game-basic-oop-pong/js/script.js
+++ exercises/broken-game-basic-oop-pong/js/script.js
@@ -51,7 +51,8 @@ function draw() {
   //////////////// FIXED added missing {
   //////////////// FIXED function reference "isOffTheScreen" to "isOffScreen"
   if (ball.isOffScreen()) {
-    reset();
+    //////////////// FIXED reference "reset()" to "ball.reset()";
+    ball.reset();
   }
 
   ball.handleCollision(leftPaddle);
